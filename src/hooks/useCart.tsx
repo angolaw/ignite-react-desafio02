0acd3e323a6fbe02a0f5b745a59aa6f8155454b2
Willian S <wsapcf@gmail.com>
2021-06-07 17:42:16 -0300

    make some tests pass on useCart hook

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,43 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+  const storagedCart =  localStorage.getItem('@RocketShoes:cart')
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
-
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart]
+      const exists = updatedCart.find(p => p.id === productId);
+    
+        const stock = await api.get(`/stock/${productId}`)
+        const stockAmount = stock.data.amount;
+        const currentAmount = exists ? exists.amount : 0;
+        const amount = currentAmount+1;
+        if(amount > stockAmount){
+          toast.error('Quantidade solicitada fora do estoque')
+          return
+        }
+        if(exists){
+          exists.amount = amount;
+        }else{
+          const product = await api.get(`/products/${productId}`)
+          const newProduct = {
+            ...product.data,
+            amount: 1
+          }
+          updatedCart.push(newProduct)
+        }
+        setCart(updatedCart)
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+      
+      
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto')
     }
   };
 
